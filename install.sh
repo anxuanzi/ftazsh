ab7eb6740128959ae2f84f02c6964c8212b72952
Tony An <anxuanzi@sh-c.cn>
2022-05-29 16:57:16 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,70 +1,60 @@
 #!/bin/bash
 
-if command -v zsh &> /dev/null && command -v git &> /dev/null && command -v wget &> /dev/null; then
-    echo -e "ZSH and Git are already installed\n"
-else
-    if sudo apt install -y zsh git wget || sudo pacman -S zsh git wget || sudo dnf install -y zsh git wget || sudo yum install -y zsh git wget || sudo brew install git zsh wget || pkg install git zsh wget ; then
-        echo -e "zsh wget and git Installed\n"
-    else
-        echo -e "Please install the following packages first, then try again: zsh git wget \n" && exit
-    fi
-fi
-
 if mv -n ~/.zshrc ~/.zshrc-backup-$(date +"%Y-%m-%d"); then # backup .zshrc
     echo -e "Backed up the current .zshrc to .zshrc-backup-date\n"
 fi
 
-mkdir -p ~/.config/ezsh       # the setup will be installed in here
+mkdir -p ~/.config/ftazsh       # the setup will be installed in here
 
 if [ -d ~/.quickzsh ]; then
     echo -e "\n PREVIOUS SETUP FOUND AT '~/.quickzsh'. PLEASE MANUALLY MOVE ANY FILES YOU'D LIKE TO '~/.config/ezsh' \n"
 fi
 
 echo -e "Installing oh-my-zsh\n"
-if [ -d ~/.config/ezsh/oh-my-zsh ]; then
+if [ -d ~/.config/ftazsh/oh-my-zsh ]; then
     echo -e "oh-my-zsh is already installed\n"
-    git -C ~/.config/ezsh/oh-my-zsh remote set-url origin https://github.com/ohmyzsh/ohmyzsh.git
+    git -C ~/.config/ftazsh/oh-my-zsh remote set-url origin https://github.com/ohmyzsh/ohmyzsh.git
 elif [ -d ~/.oh-my-zsh ]; then
-    echo -e "oh-my-zsh in already installed at '~/.oh-my-zsh'. Moving it to '~/.config/ezsh/oh-my-zsh'"
-    export ZSH="$HOME/.config/ezsh/oh-my-zsh"
-    mv ~/.oh-my-zsh ~/.config/ezsh/oh-my-zsh
-    git -C ~/.config/ezsh/oh-my-zsh remote set-url origin https://github.com/ohmyzsh/ohmyzsh.git
+    echo -e "oh-my-zsh in already installed at '~/.oh-my-zsh'. Moving it to '~/.config/ftazsh/oh-my-zsh'"
+    export ZSH="$HOME/.config/ftazsh/oh-my-zsh"
+    mv ~/.oh-my-zsh ~/.config/ftazsh/oh-my-zsh
+    git -C ~/.config/ftazsh/oh-my-zsh remote set-url origin https://github.com/ohmyzsh/ohmyzsh.git
 else
-    git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ~/.config/ezsh/oh-my-zsh
+    git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ~/.config/ftazsh/oh-my-zsh
 fi
 
 cp -f .zshrc ~/
-cp -f ezshrc.zsh ~/.config/ezsh/
+cp -f ftazshrc.zsh ~/.config/ftazsh/
 
-mkdir -p ~/.config/ezsh/zshrc         # PLACE YOUR ZSHRC CONFIGURATIONS OVER THERE
+mkdir -p ~/.config/ftazsh/zshrc         # PLACE YOUR ZSHRC CONFIGURATIONS OVER THERE
 mkdir -p ~/.cache/zsh/                # this will be used to store .zcompdump zsh completion cache files which normally clutter $HOME
 
 if [ -f ~/.zcompdump ]; then
     mv ~/.zcompdump* ~/.cache/zsh/
 fi
 
-if [ -d ~/.config/ezsh/oh-my-zsh/plugins/zsh-autosuggestions ]; then
-    cd ~/.config/ezsh/oh-my-zsh/plugins/zsh-autosuggestions && git pull
+if [ -d ~/.config/ftazsh/oh-my-zsh/plugins/zsh-autosuggestions ]; then
+    cd ~/.config/ftazsh/oh-my-zsh/plugins/zsh-autosuggestions && git pull
 else
-    git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.config/ezsh/oh-my-zsh/plugins/zsh-autosuggestions
+    git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.config/ftazsh/oh-my-zsh/plugins/zsh-autosuggestions
 fi
 
-if [ -d ~/.config/ezsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
-    cd ~/.config/ezsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting && git pull
+if [ -d ~/.config/ftazsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+    cd ~/.config/ftazsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting && git pull
 else
-    git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/ezsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+    git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/ftazsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 fi
 
-if [ -d ~/.config/ezsh/oh-my-zsh/custom/plugins/zsh-completions ]; then
-    cd ~/.config/ezsh/oh-my-zsh/custom/plugins/zsh-completions && git pull
+if [ -d ~/.config/ftazsh/oh-my-zsh/custom/plugins/zsh-completions ]; then
+    cd ~/.config/ftazsh/oh-my-zsh/custom/plugins/zsh-completions && git pull
 else
-    git clone --depth=1 https://github.com/zsh-users/zsh-completions ~/.config/ezsh/oh-my-zsh/custom/plugins/zsh-completions
+    git clone --depth=1 https://github.com/zsh-users/zsh-completions ~/.config/ftazsh/oh-my-zsh/custom/plugins/zsh-completions
 fi
 
-if [ -d ~/.config/ezsh/oh-my-zsh/custom/plugins/zsh-history-substring-search ]; then
-    cd ~/.config/ezsh/oh-my-zsh/custom/plugins/zsh-history-substring-search && git pull
+if [ -d ~/.config/ftazsh/oh-my-zsh/custom/plugins/zsh-history-substring-search ]; then
+    cd ~/.config/ftazsh/oh-my-zsh/custom/plugins/zsh-history-substring-search && git pull
 else
-    git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search ~/.config/ezsh/oh-my-zsh/custom/plugins/zsh-history-substring-search
+    git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search ~/.config/ftazsh/oh-my-zsh/custom/plugins/zsh-history-substring-search
 fi
 
 
@@ -78,55 +68,55 @@ wget -q --show-progress -N https://github.com/ryanoasis/nerd-fonts/raw/master/pa
 
 fc-cache -fv ~/.fonts
 
-if [ -d ~/.config/ezsh/oh-my-zsh/custom/themes/powerlevel10k ]; then
-    cd ~/.config/ezsh/oh-my-zsh/custom/themes/powerlevel10k && git pull
+if [ -d ~/.config/ftazsh/oh-my-zsh/custom/themes/powerlevel10k ]; then
+    cd ~/.config/ftazsh/oh-my-zsh/custom/themes/powerlevel10k && git pull
 else
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/ezsh/oh-my-zsh/custom/themes/powerlevel10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/ftazsh/oh-my-zsh/custom/themes/powerlevel10k
 fi
 
-if [ -d ~/.~/.config/ezsh/fzf ]; then
-    cd ~/.config/ezsh/fzf && git pull
-    ~/.config/ezsh/fzf/install --all --key-bindings --completion --no-update-rc
+if [ -d ~/.~/.config/ftazsh/fzf ]; then
+    cd ~/.config/ftazsh/fzf && git pull
+    ~/.config/ftazsh/fzf/install --all --key-bindings --completion --no-update-rc
 else
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.config/ezsh/fzf
-    ~/.config/ezsh/fzf/install --all --key-bindings --completion --no-update-rc
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.config/ftazsh/fzf
+    ~/.config/ftazsh/fzf/install --all --key-bindings --completion --no-update-rc
 fi
 
-if [ -d ~/.config/ezsh/oh-my-zsh/custom/plugins/k ]; then
-    cd ~/.config/ezsh/oh-my-zsh/custom/plugins/k && git pull
+if [ -d ~/.config/ftazsh/oh-my-zsh/custom/plugins/k ]; then
+    cd ~/.config/ftazsh/oh-my-zsh/custom/plugins/k && git pull
 else
-    git clone --depth 1 https://github.com/supercrabtree/k ~/.config/ezsh/oh-my-zsh/custom/plugins/k
+    git clone --depth 1 https://github.com/supercrabtree/k ~/.config/ftazsh/oh-my-zsh/custom/plugins/k
 fi
 
-if [ -d ~/.config/ezsh/marker ]; then
-    cd ~/.config/ezsh/marker && git pull
+if [ -d ~/.config/ftazsh/marker ]; then
+    cd ~/.config/ftazsh/marker && git pull
 else
-    git clone --depth 1 https://github.com/jotyGill/marker ~/.config/ezsh/marker
+    git clone --depth 1 https://github.com/jotyGill/marker ~/.config/ftazsh/marker
 fi
 
-if ~/.config/ezsh/marker/install.py; then
+if ~/.config/ftazsh/marker/install.py; then
     echo -e "Installed Marker\n"
 else
     echo -e "Marker Installation Had Issues\n"
 fi
 
-# if git clone --depth 1 https://github.com/todotxt/todo.txt-cli.git ~/.config/ezsh/todo; then :
+# if git clone --depth 1 https://github.com/todotxt/todo.txt-cli.git ~/.config/ftazsh/todo; then :
 # else
-#     cd ~/.config/ezsh/todo && git fetch --all && git reset --hard origin/master
+#     cd ~/.config/ftazsh/todo && git fetch --all && git reset --hard origin/master
 # fi
-# mkdir ~/.config/ezsh/todo/bin ; cp -f ~/.config/ezsh/todo/todo.sh ~/.config/ezsh/todo/bin/todo.sh # cp todo.sh to ./bin so only it is included in $PATH
+# mkdir ~/.config/ftazsh/todo/bin ; cp -f ~/.config/ftazsh/todo/todo.sh ~/.config/ftazsh/todo/bin/todo.sh # cp todo.sh to ./bin so only it is included in $PATH
 # #touch ~/.todo/config     # needs it, otherwise spits error , yeah a bug in todo
-# ln -s ~/.config/ezsh/todo ~/.todo
-if [ ! -L ~/.config/ezsh/todo/bin/todo.sh ]; then
-    echo -e "Installing todo.sh in ~/.config/ezsh/todo\n"
-    mkdir -p ~/.config/ezsh/bin
-    mkdir -p ~/.config/ezsh/todo
-    wget -q --show-progress "https://github.com/todotxt/todo.txt-cli/releases/download/v2.12.0/todo.txt_cli-2.12.0.tar.gz" -P ~/.config/ezsh/
-    tar xvf ~/.config/ezsh/todo.txt_cli-2.12.0.tar.gz -C ~/.config/ezsh/todo --strip 1 && rm ~/.config/ezsh/todo.txt_cli-2.12.0.tar.gz
-    ln -s -f ~/.config/ezsh/todo/todo.sh ~/.config/ezsh/bin/todo.sh     # so only .../bin is included in $PATH
-    ln -s -f ~/.config/ezsh/todo/todo.cfg ~/.todo.cfg     # it expects it there or ~/todo.cfg or ~/.todo/config
+# ln -s ~/.config/ftazsh/todo ~/.todo
+if [ ! -L ~/.config/ftazsh/todo/bin/todo.sh ]; then
+    echo -e "Installing todo.sh in ~/.config/ftazsh/todo\n"
+    mkdir -p ~/.config/ftazsh/bin
+    mkdir -p ~/.config/ftazsh/todo
+    wget -q --show-progress "https://github.com/todotxt/todo.txt-cli/releases/download/v2.12.0/todo.txt_cli-2.12.0.tar.gz" -P ~/.config/ftazsh/
+    tar xvf ~/.config/ftazsh/todo.txt_cli-2.12.0.tar.gz -C ~/.config/ftazsh/todo --strip 1 && rm ~/.config/ftazsh/todo.txt_cli-2.12.0.tar.gz
+    ln -s -f ~/.config/ftazsh/todo/todo.sh ~/.config/ftazsh/bin/todo.sh     # so only .../bin is included in $PATH
+    ln -s -f ~/.config/ftazsh/todo/todo.cfg ~/.todo.cfg     # it expects it there or ~/todo.cfg or ~/.todo/config
 else
-    echo -e "todo.sh is already instlled in ~/.config/ezsh/todo/bin/\n"
+    echo -e "todo.sh is already instlled in ~/.config/ftazsh/todo/bin/\n"
 fi
 
 if [[ $1 == "--cp-hist" ]] || [[ $1 == "-c" ]]; then
@@ -146,7 +136,7 @@ else
     echo -e "\nNot copying bash_history to zsh_history, as --cp-hist or -c is not supplied\n"
 fi
 
-ZDOTDIR="~/.config/ezsh/zshrc"
+ZDOTDIR="~/.config/ftazsh/zshrc"
 if [ ! -d $ZDOTDIR ]; then
     mkdir -p $ZDOTDIR
 fi
