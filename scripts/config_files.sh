2b6bb6b11c06291603f82a2713e9e2b0a72caebd
Tony An <anxuanzi@gmail.com>
2025-06-16 15:38:27 -0700

    Simplify path handling in `config_files.sh` by removing redundant `dirname` calls.

--- scripts/config_files.sh
+++ scripts/config_files.sh
@@ -9,15 +9,15 @@ copy_config_files() {
     print_message "Copying configuration files\n"
     
     # Copy main configuration files
-    cp -f "$(dirname "$0")/../.zshrc" ~/                                # Main .zshrc file
-    cp -f "$(dirname "$0")/../ftazshrc.zsh" ~/.config/ftazsh/           # ftazsh main configuration
-    cp -f "$(dirname "$0")/../p10k.zsh" ~/.config/ftazsh/               # Powerlevel10k theme configuration
+    cp -f "../.zshrc" ~/                                # Main .zshrc file
+    cp -f "../ftazshrc.zsh" ~/.config/ftazsh/           # ftazsh main configuration
+    cp -f "../p10k.zsh" ~/.config/ftazsh/               # Powerlevel10k theme configuration
     
     print_success "Main configuration files copied\n"
     
     # Copy example configuration
     mkdir -p ~/.config/ftazsh/zshrc                                     # User's personal ZSH configurations go here
-    cp -f "$(dirname "$0")/../personal_rc.zsh" ~/.config/ftazsh/zshrc/personal_rc.zsh  # Example configuration
+    cp -f "../personal_rc.zsh" ~/.config/ftazsh/zshrc/personal_rc.zsh  # Example configuration
     
     print_success "Personal configuration copied\n"
 }
